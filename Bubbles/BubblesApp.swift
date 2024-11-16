4044fa18df573a39c1ccf5b7648ddb69d32fe6fb
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2024-11-16 13:08:52 -0300

    Initial Commit

---
+++ Bubbles/BubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by Marcelo Sampaio on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
